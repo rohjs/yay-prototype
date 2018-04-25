0c98381c81cf2cd09e44ea9040794386cdf232e1
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-04-25 13:11:00 +0900

    Change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
+  root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
