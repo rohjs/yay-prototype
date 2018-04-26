9a8ceb2045e3e7ef30efb61064c9441f3d84892c
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-04-26 22:57:31 +0900

    Change the name of StaticPagesController and relevant files  from static-pages to homes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,4 @@
-class StaticPagesController < ApplicationController
+class HomesController < ApplicationController
   def index
     if user_signed_in?
       redirect_to dashboard_path(current_user.id)
