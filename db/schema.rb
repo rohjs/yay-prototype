620f3f0cedb5e5030ab0c5c98b26df5865287252
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-04-27 01:05:03 +0900

    Create CoursesController and corresponding view templates and etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426123448) do
+ActiveRecord::Schema.define(version: 20180426154608) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "category"
+    t.integer "level"
+    t.integer "credit"
+    t.integer "capacity"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "closing_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dashboards", force: :cascade do |t|
     t.datetime "created_at", null: false
