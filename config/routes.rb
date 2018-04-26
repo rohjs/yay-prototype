8bbee8320e10bd5ce74c3c755240e1f4bfa560ba
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-04-26 22:58:19 +0900

    Change root from static-pages#index to homes#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     :controllers => { :registrations => "registrations" }
     
 
-  root "static_pages#index"
+  root "homes#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
