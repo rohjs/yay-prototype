0b7e4854ebc4e685a7a46ab3de8ebcd67142a9e5
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-04-26 22:32:36 +0900

    Implement conditional rendering depending on user_type

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,22 @@
 class DashboardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  layout "dashboard"
+  
+  def show
+    case @user.user_type
+      when 0
+        render "dashboards/students/show"
+      when 1
+        render "dashboards/teachers/show"
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
 end
