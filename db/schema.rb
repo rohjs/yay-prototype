5a6b47ec83c25a1bb8b8f673310a01b32f38447e
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-04-26 22:33:53 +0900

    Update Dashboard model class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426104408) do
+ActiveRecord::Schema.define(version: 20180426123448) do
+
+  create_table "dashboards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
