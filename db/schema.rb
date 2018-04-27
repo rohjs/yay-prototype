07509c1c305ed6e5c08478a7c246708cfe8067b2
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-04-27 17:40:11 +0900

    Create models and generate migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,26 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426154608) do
+ActiveRecord::Schema.define(version: 20180427083921) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "requirement_id"
+    t.boolean "status", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["requirement_id"], name: "index_assignments_on_requirement_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -20,6 +41,15 @@ ActiveRecord::Schema.define(version: 20180426154608) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "requirements", force: :cascade do |t|
+    t.integer "course_id"
+    t.string "title"
+    t.string "description"
+    t.boolean "status", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_requirements_on_course_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
