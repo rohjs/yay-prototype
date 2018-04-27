2a2adce6a2438ebfd79fea3dd0120eb51dea5295
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-04-27 14:00:25 +0900

    Implement conditional rendering after sign_up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,23 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  def create
+    super
+    
+    if resource.save
+      case resource.user_type
+        when 0
+          @student = Student.new()
+          @student.user_id = resource.id
+          @student.save
+        when 1
+          @teacher = Teacher.new()
+          @teacher.user_id = resource.id
+          @teacher.save
+      end
+    end
+  end
+
   protected
 
     def user_params
