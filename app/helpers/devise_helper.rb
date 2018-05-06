cca4d4baa3a23eea3cce4b0751928721ddf75061
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-05-06 10:57:52 +0900

    Update error styles

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,8 @@ module DeviseHelper
   def devise_error_messages!
     return '' if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:h1, msg) }.join
+    messages = content_tag(:h1, resource.errors.full_messages.join(" "))
+    # messages = resource.errors.full_messages.map { |msg| content_tag(:h1, msg) }.join
     html = <<-HTML
     <aside class="error"
       id="message--error"
