9d60072bdc02caea1ca8aa51c7a4c7924d26e0fb
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-05-06 10:31:16 +0900

    Update adding new requirement item order

---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#requirements a.add_fields")
+    .data("association-insertion-method", 'append')
+    .data("association-insertion-node", "#requirement-list")
+    .on("cocoon:before-insert", function(e, elm_to_be_added) {
+        elm_to_be_added.fadeIn("slow");
+      })
+      .on("cocoon:after-insert", function(e, added_elm) {
+        added_elm.css("background","red");
+      })
+      .on("cocoon:before-remove", function(e, elm) {
+        $(this).data("remove-timeout", 1000);
+        elm.fadeOut("slow");
+      });;
+});
