7193d7c4e546e91e6cddcd5090cd7bb59a753d9c
Woohyeon Roh <roh.woohyeon@gmail.com>
2018-05-09 20:23:23 +0900

    Fix errors...?

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180427121854) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "assignments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "requirement_id"
